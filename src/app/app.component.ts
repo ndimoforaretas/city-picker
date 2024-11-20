8bf9ed8db04b97ebb38e556e72c08f090d3838ef
Ndimofor Aretas <ndimaret@gmail.com>
2024-11-20 17:01:04 +0100

    add boilerplate, bootstrap and fontawesome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { CommonModule, NgFor, NgIf } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -6,8 +8,18 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  http = inject(HttpClient);
   title = 'city-picker';
+  city: any;
+
+  ngOnInit(): void {
+    this.http.get('https://api.zippopotam.us/de/10245').subscribe({
+      next: (response) => (this.city = response),
+      error: (err) => console.error(err),
+      complete: () => console.log('done'),
+    });
+  }
 }
